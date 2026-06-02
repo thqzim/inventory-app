88619ba211ef5ec2a9b2815bedcad39b0851f0b2
Mohammed Thazeem <sleepsqwer@gmail.com>
2026-06-01 22:44:14 -0500

    finalized inventory

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,26 @@ const app = express()
 
 app.use(cors())
 
-const inventory = [ 
-    {name: "Blood Bags", stock: 21 },
-    {name: "IV Bags", stock: 12},
-    {name: "Syringes", stock: 95}
+const inventory = [
+    {name: "Blood Bags", stock: 12},
+    {name: "IV Bags", stock: 62},
+    {name: "Pills", stock: 25}
   ]
 
-app.get("/inventory", (req, res) => {res.json(inventory)})
-app.post("/inventory", (req, res) => {
-    inventory[0].stock = inventory[0].stock - 1
+app.get(`/inventory`, (req, res) => {res.json(inventory)})
+
+app.post(`/inventory/:index/request`, (req, res) => {
+    const i = req.params.index
+    if (inventory[i].stock > 0) {
+        inventory[i].stock -= 1
+    }
     res.json(inventory)
 })
 
-app.listen(5000, () => {console.log("Server has opened at port 5000")})
+app.post(`/inventory/:index/donate`, (req, res) => {
+    const i = req.params.index
+    inventory[i].stock += 1
+    res.json(inventory)
+})
+
+app.listen(5000, () => {console.log("Server has been opened at port 5000")})
